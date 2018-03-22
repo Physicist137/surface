06d8b0d79bc01551cdf7c33185dbe9a398a05fbd
Physicist137 <physicist093@yahoo.com.br>
2018-03-22 19:09:51 -0300

    Add Ballistic Deposition

--- include/deposition.hpp
+++ include/deposition.hpp
@@ -6,6 +6,8 @@
 template <typename Integer>
 void randomDeposition(Surface<Integer>& surface, int depositions);
 
+template <typename Integer>
+void ballisticDeposition2D(Surface<Integer>& surface, int depositions);
 
 // Definition of functions -----------------------------------------
 template <typename Integer>
@@ -17,4 +19,23 @@ void randomDeposition(Surface<Integer>& surface, int depositions) {
     for (int i = 0; i < depositions; ++i) {
         ++surface[dist(gen)];
     }
+}
+
+template <typename Integer>
+void ballisticDeposition2D(Surface<Integer>& surface, int depositions) {
+	std::random_device rd;
+	std::mt19937 gen(rd());
+	
+	Integer size = surface.size();
+	std::uniform_int_distribution<Integer> dist(Integer(), static_cast<Integer>(size-1));
+
+	for (int i = 0; i < depositions; ++i) {
+		Integer site = dist(gen);
+
+		if (site == size -1) surface[site] = std::max<Integer>(surface[site-1], 1+surface[site]);
+		else if (site == 0) surface[site] = std::max<Integer>(surface[site+1], 1+surface[site]);
+		else surface[site] = std::max<Integer>(
+			std::max<Integer>(surface[site-1], 1+surface[site]), 
+			surface[site]+1);
+	}
 }
