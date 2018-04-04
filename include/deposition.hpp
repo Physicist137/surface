d6c70dbe8136febe082d68ae94dd5de10af12885
Physicist137 <physicist093@yahoo.com.br>
2018-04-04 16:18:13 -0300

    Removing the random devices from the scope of the functions

--- include/deposition.hpp
+++ include/deposition.hpp
@@ -2,6 +2,10 @@
 #include "surface.hpp"
 #include <random>
 
+// PRNG outside the function scope. -----------------------------------------
+static std::random_device rd;
+static std::mt19937 gen(rd());
+
 // Declaration of fucntions. -----------------------------------------
 template <typename Integer>
 void randomDeposition(Surface<Integer>& surface, int depositions);
@@ -12,9 +16,7 @@ void ballisticDeposition2D(Surface<Integer>& surface, int depositions);
 // Definition of functions -----------------------------------------
 template <typename Integer>
 void randomDeposition(Surface<Integer>& surface, int depositions) {
-	std::random_device rd;
-	std::mt19937 gen(rd());
-    std::uniform_int_distribution<Integer> dist(Integer(), static_cast<Integer>(surface.size()-1));
+	std::uniform_int_distribution<Integer> dist(Integer(), static_cast<Integer>(surface.size()-1));
 
     for (int i = 0; i < depositions; ++i) {
         ++surface[dist(gen)];
@@ -23,16 +25,13 @@ void randomDeposition(Surface<Integer>& surface, int depositions) {
 
 template <typename Integer>
 void ballisticDeposition2D(Surface<Integer>& surface, int depositions) {
-	std::random_device rd;
-	std::mt19937 gen(rd());
-	
 	Integer size = surface.size();
 	std::uniform_int_distribution<Integer> dist(Integer(), static_cast<Integer>(size-1));
 
 	for (int i = 0; i < depositions; ++i) {
 		Integer site = dist(gen);
 
-		if (site == size -1) surface[site] = std::max<Integer>(surface[site-1], 1+surface[site]);
+		if (site == size-1) surface[site] = std::max<Integer>(surface[site-1], 1+surface[site]);
 		else if (site == 0) surface[site] = std::max<Integer>(surface[site+1], 1+surface[site]);
 		else surface[site] = std::max<Integer>(
 			std::max<Integer>(surface[site-1], 1+surface[site]), 
